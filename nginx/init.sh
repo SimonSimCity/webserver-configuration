034b58a8145141944213e013742409d3449ce697
Simon Schick <simonsimcity@gmail.com>
2011-09-05 23:14:30 +0200

    Fix misspelling in symlink-name

--- nginx/init.sh
+++ nginx/init.sh
@@ -7,8 +7,8 @@ aptitude install nginx
 
 cp $SCRIPT_PATH/conf/* /etc/nginx/ -R
 
-ln -s /etc/nginx/sites-avaliable/ssc /etc/nginx/sites-enabled/
-ln -s /etc/nginx/sites-avaliable/sf2 /etc/nginx/sites-enabled/
-ln -s /etc/nginx/sites-avaliable/sf /etc/nginx/sites-enabled/
+ln -s /etc/nginx/sites-available/ssc /etc/nginx/sites-enabled/
+ln -s /etc/nginx/sites-available/sf2 /etc/nginx/sites-enabled/
+ln -s /etc/nginx/sites-available/sf /etc/nginx/sites-enabled/
 
 /etc/init.d/nginx restart
