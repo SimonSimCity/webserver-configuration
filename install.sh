5d160ce75220c6a5ccbc71e6b1c252bb08908c83
Simon Schick <simonsimcity@gmail.com>
2011-09-05 23:10:34 +0200

    Add new configuration to set nginx index-direction globally /var/www folder should be mounted before webservers are installed.

--- install.sh
+++ install.sh
@@ -14,13 +14,14 @@ aptitude install htop
 aptitude install php5-cli php5-fpm php5-mysql mysql-server graphicsmagick
 /etc/init.d/php5-fpm restart
 
+echo Please insert the password for the MySQL user "root"
 mysql -u root -p < ./mysql/init.sql
+
+rm /var/www -R
+ln -s /mnt/www /var/www
 
 echo Install and configure nginx
 ./nginx/init.sh
 
 echo Install and configure Apache2
 ./apache/init.sh
-
-rm /var/www -R
-ln -s /mnt/www /var/www
