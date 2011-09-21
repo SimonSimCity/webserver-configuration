ec6b3da1d202a784a747cc3d0d1a954c959f463d
Simon Schick <simonsimcity@gmail.com>
2011-09-21 13:44:19 +0200

    Keep the script silent during installation so you'll only see error-messages
    Signed-off-by: Simon Schick <simonsimcity@gmail.com>

--- apache/init.sh
+++ apache/init.sh
@@ -3,10 +3,10 @@
 SCRIPT_PATH="$( cd "$(dirname "$0")" && pwd )";
 
 # Use Apache as webserver if the repository contains .htaccess files
-aptitude install apache2 libapache2-mod-fastcgi
-a2enmod actions
-a2enmod vhost_alias
+aptitude -y install apache2 libapache2-mod-fastcgi > /dev/null
+a2enmod actions > /dev/null
+a2enmod vhost_alias > /dev/null
 
 cp $SCRIPT_PATH/conf/* /etc/apache2/ -R
 
-/etc/init.d/apache2 restart
+/etc/init.d/apache2 restart > /dev/null
--- apt/register-dotdeb.sh
+++ apt/register-dotdeb.sh
@@ -6,9 +6,9 @@ echo "deb http://packages.dotdeb.org stable all" >> /etc/apt/sources.list
 echo "deb-src http://packages.dotdeb.org stable all" >> /etc/apt/sources.list
 
 # Load public-key of this package-repository
-wget http://www.dotdeb.org/dotdeb.gpg
+wget http://www.dotdeb.org/dotdeb.gpg > /dev/null
 cat dotdeb.gpg | apt-key add -
 rm dotdeb.gpg
 
-aptitude update
-aptitude upgrade
+aptitude -y update > /dev/null
+aptitude -y upgrade > /dev/null
