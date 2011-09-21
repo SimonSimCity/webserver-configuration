c4817ee9dfedd672b73a5d1f6f18a3a5e35a42c6
Simon Schick <simonsimcity@gmail.com>
2011-09-21 13:31:28 +0200

    User can choose wether he wants to have a global user without password in mysql or not
    Signed-off-by: Simon Schick <simonsimcity@gmail.com>

--- install.sh
+++ install.sh
@@ -14,8 +14,11 @@ aptitude install htop
 aptitude install php5-cli php5-fpm php5-gd php5-mysql mysql-server graphicsmagick
 /etc/init.d/php5-fpm restart
 
-echo Please insert the password for the MySQL user "root"
-mysql -u root -p < ./mysql/init.sql
+read -p "Do you want to have a mysql-user %everyone% using no password? (y|n) " -n 1
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	mysql -u root -p < ./mysql/init.sql
+fi
 
 # create directory for logs
 [ -a /var/www/log ] || mkdir /var/www/log
