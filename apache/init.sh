ff9a3f1efbc7f895b18d9182d6ccea854dc38db0
Simon Schick <simonsimcity@gmail.com>
2011-09-21 09:01:11 +0200

    Added dynamic-vhost configuration to Apache2
    Signed-off-by: Simon Schick <simonsimcity@gmail.com>

--- apache/init.sh
+++ apache/init.sh
@@ -5,6 +5,7 @@ SCRIPT_PATH="$( cd "$(dirname "$0")" && pwd )";
 # Use Apache as webserver if the repository contains .htaccess files
 aptitude install apache2 libapache2-mod-fastcgi
 a2enmod actions
+a2enmod vhost_alias
 
 cp $SCRIPT_PATH/conf/* /etc/apache2/ -R
 
