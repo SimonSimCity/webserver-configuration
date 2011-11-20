cbc1166fae3f84cdd64fffda2b4c16b9f40ac639
Simon Schick <simonsimcity@gmail.com>
2011-11-20 19:43:30 +0100

    Refactor samba-mount to use the fstab instead of own-scripting in /etc/rc.local Removing cifs workarround for smbfs

--- samba/init.sh
+++ samba/init.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-read -p "Command to recive the resources ip-address f.e. \"wins_getipaddress MYWINSHOST 192.168.56\" [ \"echo 192.168.56.1\" ]" host
-if [ -z "$host" ]; then
-	host="echo 192.168.56.1"
+# Set the apache default www-root.
+wwwroot="/var/www"
+
+read -p "File system: [ \"//192.168.56.1/htdocs\" ]" fs
+if [ -z "$fs" ]; then
+	fs="echo 192.168.56.1"
 fi
 
-read -p "Have you used the script wins_getipaddress in the last step? (y/n) [ \"n\" ]"
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-	cp wins_getipaddress.sh /sbin/
+read -p "Mountpoint: [ \"/mnt/www\" ]" mnt
+if [ -z "$path" ]; then
+  mnt="/mnt/www"
 fi
 
 read -p "Username: " username
@@ -15,54 +18,33 @@ read -s -p "Password: " password
 # If you're using a read-command and unhide the user-input no new-line will be added after the command. So I do it manually
 echo
 
-read -p "Path to mount the external ressource [ \"/mnt/www\" ]" path
-if [ -z "$path" ]; then
-  path="/mnt/www"
-fi
-
 # create directory if not exists or clean it up.
-if [[ -a /var/www/ ]]; then
-	read -p "Currently there are some files in /var/www/. We need to remove them. Do you want to proceed (y/n)?"
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		rm /var/www/* -R
-	else
-		exit;
-	fi
+if [ -d "$wwwroot" ]; then
+	read -p "The directory $wwwroot exists. The script will not create a symlink pointing to $mnt. You now have the possiblity to do it on another screen. Hit ENTER to continue."
 else
-	mkdir /var/www/
+	# create symlink from the mounted directory to $wwwroot
+	ln -s $mnt $wwwroot
 fi
 
-# create symlink from the mounted directory to /var/www/
-ln -s $path /var/www
-
-# Install the samba-client for mount
+echo Install smbfs
 aptitude -y install smbfs > /dev/null
 
-# create directory if not exists
-[ -a $path ] || mkdir $path
+# create directory, where the resource should be mounted, if not exists
+[ -a $mnt ] || mkdir $mnt
 
-# Add the command to load the external resource to rc.local
-echo "" >> /etc/rc.local
-echo "srv=\`$host\`" >> /etc/rc.local
-echo "" >> /etc/rc.local
-echo "# mount the webfolder" >> /etc/rc.local
-echo "# nobrl parameter is important - see http://www.kitpages.fr/sf2_samba_sqlite3.php" >> /etc/rc.local
-echo "mount -t smbfs -o username=$username,password=$password,uid=www-data,gid=www-data,nobrl //\$srv/htdocs $path" >> /etc/rc.local
-echo "" >> /etc/rc.local
-echo "# cifs client workaround" >> /etc/rc.local
-echo "# @see: http://blog.dhampir.no/content/cifs-vfs-no-response-for-cmd-n-mid" >> /etc/rc.local
-echo "modprobe cifs" >> /etc/rc.local
-echo "echo 0 > /proc/fs/cifs/OplockEnabled" >> /etc/rc.local
+# Add the setting to load the external resource
+echo "" >> /etc/fstab
+echo "# nobrl parameter is important - see http://www.kitpages.fr/sf2_samba_sqlite3.php" >> /etc/fstab
+echo "$fs $mnt smbfs username=$username,password=$password,uid=www-data,gid=www-data,nobrl 0 0" >> /etc/fstab
 
 echo
-echo "This script has changed some settings in the file /etc/rc.local"
-echo "Ensure that the commands can be executed. In my case I allways have to move a \"exit 0\" line."
+echo "This script has appended three lines to the file /etc/fstab"
 read -p "Do you want to edit the file now? (y/n)"
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-	editor /etc/rc.local
+	editor /etc/fstab
 fi
 
-read -p "Do you want to call the script /etc/rc.local now? (y/n)"
+read -p "Do you want to mount the samba resource now by reloading /etc/fstab? (y/n)"
 if [[ $REPLY =~ ^[Yy]$ ]]; then
-	/etc/rc.local > /dev/null
+	mount -a
 fi
