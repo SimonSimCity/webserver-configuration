64044d5919c69049d579f39287dce76347179234
Simon Schick <simonsimcity@gmail.com>
2012-10-23 11:35:17 +0200

    Added PHP extensions intl and sqlite

--- php/init.sh
+++ php/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 SCRIPT_PATH="$( cd "$(dirname "$0")" && pwd )";
 
-aptitude -y install php5-apc php5-cli php5-curl php5-fpm php5-gd php5-mcrypt php5-mysql php5-xdebug > /dev/null
+aptitude -y install php5-apc php5-cli php5-curl php5-fpm php5-gd php5-mcrypt php5-mysql php5-xdebug php5-intl php5-sqlite > /dev/null
 patch -u -p0 -d /etc/php5 < $SCRIPT_PATH/patch.diff
 
 /etc/init.d/php5-fpm restart > /dev/null
